37515a3bc0a7f662433801fce409e45a0145ea23
GodaniShubham <godanishubham30@gmail.com>
2025-08-29 23:57:03 +0530

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
+
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+
 
 app_name = "accounts"
 
@@ -13,4 +16,30 @@ urlpatterns = [
     # Forgot & Reset Password
     path("forgot-password/", views.forgot_password_view, name="forgot_password"),
     path("reset-password/<uidb64>/<token>/", views.reset_password_view, name="reset_password"),
+    path('forgot-password/', 
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset.html"
+        ), 
+        name='password_reset'),
+
+    # Email sent confirmation
+    path('forgot-password/done/', 
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ), 
+        name='password_reset_done'),
+
+    # Link in email (confirm new password)
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html"
+        ), 
+        name='password_reset_confirm'),
+
+    # Password successfully reset
+    path('reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ), 
+        name='password_reset_complete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -134,3 +134,25 @@ def reset_password_view(request, uidb64, token):
     else:
         messages.error(request, "Invalid or expired password reset link.")
         return redirect("accounts:forgot_password")
+
+
+from django.urls import reverse
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+
+def forgot_password_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        try:
+            user = User.objects.get(email=email)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = default_token_generator.make_token(user)
+            reset_url = request.build_absolute_uri(
+                reverse("password_reset_confirm", kwargs={"uidb64": uid, "token": token})
+            )
+            # Send email with reset_url
+            ...
+        except User.DoesNotExist:
+            pass
+    return render(request, "accounts/forgot_password.html")
